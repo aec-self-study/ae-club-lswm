ef7006f713a1cbc9b77a1d3bcd4248ed5f543450
MistressMetal <57451414+MistressMetal@users.noreply.github.com>
2023-06-23 13:36:32 +0000

    Add customers query

---
+++ customers.sql
@@ -0,0 +1,7 @@
+SELECT customer_id, name, min(created_at) as first_order_at, count(customer_id) as number_of_orders
+from `analytics-engineers-club.coffee_shop.customers` AS customers
+LEFT JOIN `analytics-engineers-club.coffee_shop.orders` AS orders
+ON orders.customer_id = customers.id
+GROUP BY customer_id, name
+ORDER BY first_order_at 
+LIMIT 5;
